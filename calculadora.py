e028ffe8e1d51389593524780ed02e3077a6e239
BrunnaSoares <brunnag712@gmail.com>
2025-04-27 19:40:52 -0300

    my first commit

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+num1 = 0
+num2 = 0
+resultado = 0
+operacao = ''
+while True:
+    num1 = float(input('Digite o primeiro número: '))
+    operacao   = input('Digite a operacao:')
+    num2 = float(input('Digite o segundo número: '))
+
+    if operacao == '+':
+        resultado = num1 + num2
+    elif operacao == '-':
+        resultado = num1 - num2
+
+                       
